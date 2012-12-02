794d422219a41992905dacddfd978baf08ee1f29
Chris Brown <xoebus@xoeb.us>
2012-12-01 21:15:37 -0800

    Made the install script prettier.

--- install.sh
+++ install.sh
@@ -1,4 +1,18 @@
-cd ~/.vim
+# Okay, let's get in the right directory here.
+pushd ~/.vim
+
+# Get our environment set up properly.
+mkdir -p ~/.vim/backup
+mkdir -p ~/.vim/undo
+mkdir -p ~/.vim/swap
+
+# Link our configuration files to the correct place
+ln -sf ~/.vim/vimrc ~/.vimrc
+ln -sf ~/.vim/gvimrc ~/.gvimrc
+
+# Update Vundle and install bundles
 git submodule update --init
-ln -s ~/.vim/vimrc ~/.vimrc
-ln -s ~/.vim/gvimrc ~/.gvimrc
+vim +BundleInstall +qall
+
+# And finally let's put the user back where they were
+popd
